b8b4cf8756479769186e1862d49ef7ee2c0aa495
Vaibhav-Ujagare <vaibhavujagare77@gmail.com>
2025-05-05 12:48:06 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is running ${port}`);
+});
+
+
