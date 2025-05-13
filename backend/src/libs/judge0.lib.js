9b81ff7677d8895dbcef95652eb3e35bf0a98b66
Vaibhav-Ujagare <vaibhavujagare77@gmail.com>
2025-05-13 23:05:53 +0530

    created helper functions for creatingProblems controller

---
+++ backend/src/libs/judge0.lib.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { ApiError } from "../utils/apiError.js";
+
+export const getJudge0LanguageId = (language) => {
+  const languageMap = {
+    PYTHON: 71,
+    JAVA: 62,
+    JAVASCRIPT: 63,
+  };
+  return languageMap[language.toUpperCase()];
+};
+
+export const submitBatch = async (submissions) => {
+  try {
+    const { data } = await axios.post(
+      `${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,
+      { submissions } // key must be `submissions`, not `submission`
+    );
+    return data;
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Error in submitBatch Function");
+  }
+};
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const pollBatchResults = async (tokens) => {
+  console.log("reached in pollBatchResults");
+  while (true) {
+    const { data } = await axios.get(
+      `${process.env.JUDGE0_API_URL}/submissions/batch`,
+      {
+        params: {
+          tokens: tokens.join(","),
+          base64_encoded: false,
+        },
+      }
+    );
+
+    const results = data.submissions;
+    const isAllDone = results.every((result) => result.status.id >= 3);
+
+
+    if (isAllDone) return results;
+
+    await sleep(1000);
+  }
+};
