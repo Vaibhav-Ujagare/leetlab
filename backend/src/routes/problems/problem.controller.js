feb5fac9d906ddf73e9fe1c5e139fb0823478b6b
Vaibhav-Ujagare <vaibhavujagare77@gmail.com>
2025-05-15 20:48:31 +0530

    getallproblem getproblembyid udpateproblembyid controller finished

--- backend/src/routes/problems/problem.controller.js
+++ backend/src/routes/problems/problem.controller.js
@@ -46,7 +46,7 @@ export const createProblem = asyncHandler(async (req, res) => {
 
       const submissionResults = await submitBatch(submissions);
       console.log("----------------------", submissionResults);
-      
+
       const tokens = submissionResults.map((res) => res.token);
 
       const results = await pollBatchResults(tokens);
@@ -92,11 +92,139 @@ export const createProblem = asyncHandler(async (req, res) => {
   }
 });
 
-export const getAllProblems = asyncHandler(async (req, res) => {});
+export const getAllProblems = asyncHandler(async (req, res) => {
+  try {
+    const problems = await db.problem.findMany();
 
-export const getProblemById = asyncHandler(async (req, res) => {});
+    if (!problems) {
+      throw new ApiError(401, "No problem found");
+    }
 
-export const updateProblemById = asyncHandler(async (req, res) => {});
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { problems }, "Problem Feteched Successfully")
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Error while fetching a problem");
+  }
+});
+
+export const getProblemById = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    if (!id) {
+      throw new ApiError(401, "Invalid problem ID");
+    }
+
+    const problem = await db.problem.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { problem }, "Problem Feteched Successfully"));
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Error while fetching a problem");
+  }
+});
+
+export const updateProblemById = asyncHandler(async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    hints,
+    editorial,
+    testCases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
+
+  const { id } = req.params;
+  const problem = await db.problem.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!problem) {
+    throw new ApiError(401, "Invalid Problem ID");
+  }
+
+  try {
+    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+      const languageId = getJudge0LanguageId(language);
+
+      if (!languageId) {
+        throw new ApiError(401, `Language ${language} is not supported`);
+      }
+
+      const submissions = testCases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_out: output,
+      }));
+      console.log(submissions);
+
+      const submissionResults = await submitBatch(submissions);
+
+      const tokens = submissionResults.map((res) => res.token);
+
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+
+        if (result.status.id !== 3) {
+          throw new ApiError(
+            401,
+            `Testcase ${i + 1} failed for language ${language}`
+          );
+        }
+      }
+
+      const updateProblem = await db.problem.update({
+        where: {
+          id,
+        },
+        data: {
+          title,
+          description,
+          difficulty,
+          tags,
+          examples,
+          constraints,
+          hints,
+          editorial,
+          testCases,
+          codeSnippets,
+          referenceSolutions,
+          userId: req.user.id,
+        },
+      });
+
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            { updateProblem },
+            "Problem Created Successfully"
+          )
+        );
+    }
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Error while updating a problem");
+  }
+});
 
 export const deleteProblemById = asyncHandler(async (req, res) => {});
 
